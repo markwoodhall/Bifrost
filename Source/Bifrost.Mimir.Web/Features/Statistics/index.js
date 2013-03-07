7d69f706453ee4c6af0b94540a2c04fe5565bf7a
markwoodhall <mark.woodhall@gmail.com>
2013-03-07 21:53:38 +0000

    #147 Add justgage charts

--- Source/Bifrost.Mimir.Web/Features/Statistics/index.js
+++ Source/Bifrost.Mimir.Web/Features/Statistics/index.js
@@ -1,2 +1,30 @@
-﻿Bifrost.features.featureManager.get("Statistics/index").defineViewModel(function () {
+﻿
+Bifrost.namespace("web.features.statistics", {
+    index: Bifrost.Type.extend(function () {
+        this.StatisticModel = {
+            Statistics: [
+                { name: "HadValidationError", value: 500, max: 10000, title: "Validation Errors", warning: true },
+                { name: "HadException", value: 50, max: 10000, title: "Exceptions", warning: true },
+                { name: "FailedSecurity", value: 523, max: 10000, title: "Failed Security", warning: true },
+            ]
+        };
+    })
 });
+
+
+Bifrost.features.featureManager.get("Statistics/index").defineViewModel(web.features.statistics.index);
+
+ko.bindingHandlers.statistic = {
+    update: function (element, valueAccessor, allBindingsAccessor, viewModel, bindingContext) {
+        console.log(viewModel);
+        var guage = new JustGage({
+            id: viewModel.name,
+            value: viewModel.value,
+            min: 0,
+            max: viewModel.max,
+            title: viewModel.title,
+            levelColorsGradient: false
+        });
+    }
+}
+
